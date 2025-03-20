abf8d7bbbb6615fa614e772d1dd68647d45cac87
vinodh1988 <k.c.vinodh@hotmail.com>
2025-03-20 10:05:00 +0530

    latest

--- routes/mongoroutes.py
+++ routes/mongoroutes.py
@@ -1496,7 +1496,7 @@ def get_spring_boot_file(fname):
     try:
             if fname == 'instructions':
                 filename = 'Project-Instructions.pdf'
-                file_path = basedir.join('uploads/spring-questions', filename)
+                file_path = basedir+'/uploads/spring-questions/'+ filename
                 if not os.path.exists(file_path):
                     return jsonify({"error": "File not found on server"}), 404
                 return send_file(file_path, as_attachment=True, download_name=filename)
@@ -1513,7 +1513,7 @@ def get_spring_boot_file(fname):
             filename = document['filename']
 
             # Construct the file path
-            file_path = basedir.join('uploads/spring-questions', filename)
+            file_path = basedir+'/uploads/spring-questions/'+ filename
 
             if not os.path.exists(file_path):
                 return jsonify({"error": "File not found on server"}), 404
